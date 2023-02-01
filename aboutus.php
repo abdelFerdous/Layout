11e6c88846dad6f353f9aea3dfbb6704355f4baa
abdelFerdous <abdel22000@student.hamk.fi>
2023-02-01 10:53:47 +0200

    aboutUsModified

--- aboutus.php
+++ aboutus.php
@@ -3,4 +3,5 @@ $title = "About us";
 include("header.php")?>
 <h1>About us</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat unde, quaerat quod odio minus quia dolorum iusto, labore sapiente ipsum ex cum ipsam? Iste unde nemo ut expedita, alias porro!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas soluta error id eius odio vel atque, sequi fugiat veniam expedita totam rem! Expedita pariatur architecto iste quia sapiente nesciunt ipsam!</p>
 <?php include("footer.php") ?>
